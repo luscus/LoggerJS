1e5d10e0e418491e1f52070a6358e23a11e199e5
Yann Lallemant <ylallemant@timocom.com>
2014-08-15 16:07:41 +0200

    modified files to support new folder structure

---
+++ test/backup/node.loggerjs.test.js
@@ -0,0 +1,46 @@
+var LoggerJS = require('../../lib/loggerjs.node'),
+    options = {
+      "namespace" :    "node.logger.test.run",
+      "status" :       true,
+      "logLevel" :     "DEBUG",
+      "tags" :         ["NODE","TEST","LOGGERJS"]
+    },
+    logger = new LoggerJS.Logger(options);
+
+
+console.log(logger);
+
+// Handle unexpected errors
+var options = {
+      name: 'error',
+      status: true,
+      strict: false,
+      logLevel : LoggerJS.ERROR,
+      task: function (logEntry) {
+        console.log('error TASK running...');
+      }
+    },
+    task = new LoggerJS.LogTask(options);
+
+logger.registerLogTask(task);
+
+logger.useLogfile('./test/backup/test.log');
+
+
+
+function runTest () {
+  for (var idx in logger.log_priority) {
+    var method = logger.log_priority[idx];
+    logger[method.toLowerCase()]('ein kleiner "'+method+'" output Test');
+  }
+
+  console.info(logger.log('some return value test'));
+
+  logger.useLogServer('http://dggfgd');
+
+
+  test.doit();
+}
+
+
+setInterval(runTest, 1000);
